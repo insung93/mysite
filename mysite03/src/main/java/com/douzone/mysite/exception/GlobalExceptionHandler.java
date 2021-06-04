9074a82d4072f67cee509cc5fb4f5b40846247e7
insung93 <jis5780@gmail.com>
2021-06-04 11:48:18 +0900

    GlobalExceptionHandler

---
+++ mysite03/src/main/java/com/douzone/mysite/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.douzone.mysite.exception;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(Exception.class)
+	public String handlerException(Model model,Exception e) {
+		// 1. 로깅(logging)
+		System.out.println(e);
+		StringWriter errors = new StringWriter();
+		e.printStackTrace(new PrintWriter(errors));
+		System.out.println(errors);
+		model.addAttribute("exception", errors.toString());
+		// 2. 사과페이지
+		// 3. 정상 종료
+		return "error/exception";
+	}
+}
--- mysite03/src/main/webapp/WEB-INF/views/error/exception.jsp
+++ mysite03/src/main/webapp/WEB-INF/views/error/exception.jsp
@@ -10,5 +10,8 @@
 </head>
 <body>
 	<h1>Exception Occurs</h1>
+	<pre style="color: blue">
+		${exception }
+	</pre>
 </body>
 </html>
